f167faf93347e52f4c819d93e3e757f37a45b6de
jiyoonlee <im.lizhilun@gmail.com>
2024-05-13 21:02:28 +0900

    프로젝트 셋팅 (Next > React)

--- dongajul-fe/README.md
+++ dongajul-fe/README.md
@@ -1,3 +1,3 @@
-## Introduce
+# React + Vite
 
 동아줄 프로젝트 FE 소스 코드입니다.
---
+++ dongajul-fe/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>메인 페이지</h1>
+    </>
+  );
+}
+
+export default App;
