2a183e14220c2f4f58ccc851707e7dcbc0b98bfb
jiyoonlee <im.lizhilun@gmail.com>
2024-05-23 00:56:49 +0900

    Fab 컴포넌트 개발

---
+++ dongajul-fe/src/components/fab/Fab.jsx
@@ -0,0 +1,12 @@
+import fav from '../../assets/icons/icon_fab.svg';
+import './Fab.scss';
+
+const Fab = ({ onClick }) => {
+  return (
+    <div className='fab' onClick={onClick}>
+      <img src={fav} alt='fab' />
+    </div>
+  );
+};
+
+export default Fab;
