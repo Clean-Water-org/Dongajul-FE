352263b31d5f8487d6ffd371fe2dcf1537204616
jiyoonlee <im.lizhilun@gmail.com>
2024-05-23 03:12:21 +0900

    기타 중간 커밋

--- dongajul-fe/src/App.jsx
+++ dongajul-fe/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import '../src/styles/style.scss';
 import { Suspense } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import RoutesComponents from './router/routes';
--- dongajul-fe/src/router/routes.jsx
+++ dongajul-fe/src/router/routes.jsx
@@ -1,13 +1,26 @@
 import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Layout from '../components/layouts/Layout';
 
 const Home = lazy(() => import('../pages/common/Home'));
+const Login = lazy(() => import('../pages/login/Login'));
 const NotFound = lazy(() => import('../pages/common/NotFound'));
+
+const routes = [
+  { key: 'home', path: '/', element: <Home /> },
+  { key: 'login', path: '/login', element: <Login /> },
+  { key: 'notfound', path: '*', element: <NotFound /> },
+];
 
 const RoutesComponents = () => (
   <Routes>
-    <Route key='home' path='/' element={<Home />} />
-    <Route key='notfound' path='*' element={<NotFound />} />
+    {routes.map(({ key, path, element }) => (
+      <Route
+        key={key}
+        path={path}
+        element={<Layout page={element} main={key === 'home'} />}
+      />
+    ))}
   </Routes>
 );
 
